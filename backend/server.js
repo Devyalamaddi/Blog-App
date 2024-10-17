cf265026abe79e1602722f088df5947b0a17806b
devyalamaddi <devendrayalamaddi@gmail.com>
2024-10-17 11:54:44 +0530

    modified port number assigning at line 7 used || operator

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = exp()
 require('dotenv').config()//process.env.PORT
 
 //assign port number
-const port=process.env.PORT;
+const port=process.env.PORT || 5000;
 app.listen(port,()=>console.log(`web server running on port ${port}`));
